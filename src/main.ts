e332ae37acdcb6e08532427999f77348d1655374
Insiro <leeon@insiro.me>
2022-07-22 15:21:55 +0900

    add: express-session

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import 'reflect-metadata';
+import * as session from 'express-session';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
+    app.use(
+      session({ secret: 'nado123@', resave: false, saveUninitialized: false }),
+    );
     await app.listen(3000);
     console.log('initialize nado writer server');
   } catch (e) {}
