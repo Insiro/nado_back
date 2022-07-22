56a6078c0d59c0026e7ee9e84fb0424ffdac0e98
Insiro <leeon@insiro.me>
2022-07-22 15:19:19 +0900

    refactor: move app module path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './post/post.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { CommentModule } from './comment/comment.module';
+import { PostModule } from '../post/post.module';
+import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
+import { CommentModule } from '../comment/comment.module';
 import { DatabaseConfig } from './database.config';
 
 const typeormOption = {
