a260d6677c39d64550014807bd2cc8f6ccf225a1
Insiro <leeon@insiro.me>
2022-08-14 00:40:48 +0900

    add: comment controller and service (not tested)

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Session,
+} from '@nestjs/common';
+
+import { SessionType } from '../utils';
+
+import { CommentService } from './comment.service';
+import { UserService } from '../user/user.service';
+import { EditableCommentDto } from './comment.dto';
+
+import Comment from '../entities/comment.entity';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+  constructor(
+    readonly commentService: CommentService,
+    readonly userService: UserService,
+  ) {}
+
+  @Get('id')
+  async getComment(@Param('id') commentId: string): Promise<Comment> {
+    return await this.commentService.getOne(commentId);
+  }
+
+  @Put('id')
+  async fixComment(
+    @Session() session: SessionType,
+    @Param('id') commentId: string,
+    @Body() commentOpt: EditableCommentDto,
+  ) {
+    const user = await this.userService.getSigned(session);
+    await this.commentService.updateComment(user, commentId, commentOpt);
+  }
+
+  @Post('id')
+  async appendComment(
+    @Session() session: SessionType,
+    @Param('id') commentId: string,
+    @Body() commentOpt: EditableCommentDto,
+  ) {
+    const user = await this.userService.getSigned(session);
+    await this.commentService.addComment(user, commentOpt, {
+      parent: commentId,
+    });
+  }
+
+  @Delete('id')
+  async deleteComment(
+    @Session() session: SessionType,
+    @Param('id') commentId: string,
+  ) {
+    const user = await this.userService.getSigned(session);
+    await this.commentService.deleteComment(user, commentId);
+  }
+}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,11 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import User from '../entities/user.entity';
 import { Repository } from 'typeorm';
+
+import Comment from '../entities/comment.entity';
+import User from '../entities/user.entity';
+import Posts from '../entities/post.entity';
+
+import { EditableCommentDto } from './comment.dto';
 
 @Injectable()
 export class CommentService {
   constructor(
-    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Comment) private commentRepository: Repository<Comment>,
   ) {}
+
+  async getOne(id: string): Promise<Comment> {
+    const comment = await this.commentRepository.findOne({ where: { id: id } });
+    if (comment == null) throw new NotFoundException();
+    return comment;
+  }
+
+  async addComment(
+    user: User,
+    commentOpt: EditableCommentDto,
+    option: {
+      parent?: string;
+      post?: Posts;
+    },
+  ) {
+    const comment: Omit<Comment, 'id'> = {
+      author: user,
+      parent: null,
+      post: null,
+      ...commentOpt,
+    };
+    if (option.parent) {
+      comment.parent = await this.getOne(option.parent);
+      comment.post = comment.parent.post;
+    } else if (option.post) {
+    } else throw new UnprocessableEntityException();
+
+    try {
+      await this.commentRepository.insert(comment);
+    } catch (_) {
+      throw new UnprocessableEntityException();
+    }
+  }
+
+  async deleteComment(user: User, commentId: string) {
+    const comment = await this.getOne(commentId);
+    if (user.uid !== comment.author.uid) throw new UnauthorizedException();
+    try {
+      await this.commentRepository.delete(comment);
+    } catch (e) {
+      throw new UnprocessableEntityException();
+    }
+  }
+
+  async updateComment(
+    user: User,
+    commentId: string,
+    commentOpt: EditableCommentDto,
+  ) {
+    const comment = await this.getOne(commentId);
+    if (user.uid !== comment.author.uid) throw new UnauthorizedException();
+    try {
+      comment.content = commentOpt.content;
+      await this.commentRepository.save(comment);
+    } catch (e) {
+      throw new UnprocessableEntityException();
+    }
+  }
+
+  async getByPosts(post: Posts): Promise<Comment[]> {
+    return await this.commentRepository.find({
+      where: { post: post },
+    });
+  }
+
+  async getByAuthor(user: User): Promise<Comment[]> {
+    return await this.commentRepository.find({
+      where: { author: user },
+    });
+  }
 }
